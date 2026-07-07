cff5d0be51ad8bee0a888334dca4ab02e5198db2
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 12:05:53 +0530

    feat(phase3): expose persistent incident store

--- aegis/correlation/__init__.py
+++ aegis/correlation/__init__.py
@@ -1,11 +1,12 @@
 from aegis.correlation.engine import CorrelationEdge, CorrelationEngine, CorrelationEvidence, CorrelationWeights
 from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
+from aegis.correlation.incident_store import IncidentNotFoundError, PersistentIncidentStore
 from aegis.correlation.incidents import Incident, IncidentClusterer, IncidentLifecycle
 from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, AssetType, RelationshipType
 
 __all__ = [
     "CorrelationEdge", "CorrelationEngine", "CorrelationEvidence", "CorrelationWeights",
     "AlertEnricher", "DetectorEvidence", "EnrichedAlert",
-    "Incident", "IncidentClusterer", "IncidentLifecycle",
+    "Incident", "IncidentClusterer", "IncidentLifecycle", "PersistentIncidentStore", "IncidentNotFoundError",
     "Asset", "AssetRelationship", "AssetTopology", "AssetType", "RelationshipType",
 ]
