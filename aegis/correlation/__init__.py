e4d59faca274a487f74606ab59e4caa87f5b23b9
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 12:29:46 +0530

    feat(phase3): expose attack graph engine

--- aegis/correlation/__init__.py
+++ aegis/correlation/__init__.py
@@ -1,3 +1,4 @@
+from aegis.correlation.attack_graph import AttackGraph, AttackGraphEdge, AttackGraphEngine, AttackGraphNode, GraphEdgeType, GraphNodeType
 from aegis.correlation.engine import CorrelationEdge, CorrelationEngine, CorrelationEvidence, CorrelationWeights
 from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
 from aegis.correlation.incident_store import IncidentNotFoundError, PersistentIncidentStore
@@ -5,6 +6,7 @@ from aegis.correlation.incidents import Incident, IncidentClusterer, IncidentLif
 from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, AssetType, RelationshipType
 
 __all__ = [
+    "AttackGraph", "AttackGraphEdge", "AttackGraphEngine", "AttackGraphNode", "GraphEdgeType", "GraphNodeType",
     "CorrelationEdge", "CorrelationEngine", "CorrelationEvidence", "CorrelationWeights",
     "AlertEnricher", "DetectorEvidence", "EnrichedAlert",
     "Incident", "IncidentClusterer", "IncidentLifecycle", "PersistentIncidentStore", "IncidentNotFoundError",
