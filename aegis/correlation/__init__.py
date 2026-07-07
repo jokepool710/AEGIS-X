a490b8ca2c293c8c54549120b43fd3d2c54e2582
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 12:01:20 +0530

    feat(phase3): expose incident clustering primitives

--- aegis/correlation/__init__.py
+++ aegis/correlation/__init__.py
@@ -1,9 +1,11 @@
 from aegis.correlation.engine import CorrelationEdge, CorrelationEngine, CorrelationEvidence, CorrelationWeights
 from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
+from aegis.correlation.incidents import Incident, IncidentClusterer, IncidentLifecycle
 from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, AssetType, RelationshipType
 
 __all__ = [
     "CorrelationEdge", "CorrelationEngine", "CorrelationEvidence", "CorrelationWeights",
     "AlertEnricher", "DetectorEvidence", "EnrichedAlert",
+    "Incident", "IncidentClusterer", "IncidentLifecycle",
     "Asset", "AssetRelationship", "AssetTopology", "AssetType", "RelationshipType",
 ]
