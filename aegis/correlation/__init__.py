f5efea45de429f51253fc8681f921c3720b69464
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 11:57:00 +0530

    feat(phase3): expose correlation engine primitives

--- aegis/correlation/__init__.py
+++ aegis/correlation/__init__.py
@@ -1,13 +1,9 @@
+from aegis.correlation.engine import CorrelationEdge, CorrelationEngine, CorrelationEvidence, CorrelationWeights
 from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
 from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, AssetType, RelationshipType
 
 __all__ = [
-    "AlertEnricher",
-    "DetectorEvidence",
-    "EnrichedAlert",
-    "Asset",
-    "AssetRelationship",
-    "AssetTopology",
-    "AssetType",
-    "RelationshipType",
+    "CorrelationEdge", "CorrelationEngine", "CorrelationEvidence", "CorrelationWeights",
+    "AlertEnricher", "DetectorEvidence", "EnrichedAlert",
+    "Asset", "AssetRelationship", "AssetTopology", "AssetType", "RelationshipType",
 ]
