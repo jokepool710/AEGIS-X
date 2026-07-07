1490ddbc4f24c3f8d43989112b45fc88aea8dbf9
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 11:55:08 +0530

    feat(phase3): expose correlation topology primitives

---
+++ aegis/correlation/__init__.py
@@ -0,0 +1,13 @@
+from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
+from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, AssetType, RelationshipType
+
+__all__ = [
+    "AlertEnricher",
+    "DetectorEvidence",
+    "EnrichedAlert",
+    "Asset",
+    "AssetRelationship",
+    "AssetTopology",
+    "AssetType",
+    "RelationshipType",
+]
