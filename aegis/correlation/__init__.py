943b62a43621bfa97022cdae531c9ed2a38d2d17
Tanishq Mahajan <72680450+jokepool710@users.noreply.github.com>
2026-07-07 12:50:15 +0530

    feat(phase3): expose ATT&CK for ICS mapper

--- aegis/correlation/__init__.py
+++ aegis/correlation/__init__.py
@@ -1,4 +1,5 @@
 from aegis.correlation.attack_graph import AttackGraph, AttackGraphEdge, AttackGraphEngine, AttackGraphNode, GraphEdgeType, GraphNodeType
+from aegis.correlation.attack_mapping import ICSAttackMapper, ICSMapping, MappingRule
 from aegis.correlation.engine import CorrelationEdge, CorrelationEngine, CorrelationEvidence, CorrelationWeights
 from aegis.correlation.enrichment import AlertEnricher, DetectorEvidence, EnrichedAlert
 from aegis.correlation.incident_store import IncidentNotFoundError, PersistentIncidentStore
@@ -7,6 +8,7 @@ from aegis.correlation.topology import Asset, AssetRelationship, AssetTopology, 
 
 __all__ = [
     "AttackGraph", "AttackGraphEdge", "AttackGraphEngine", "AttackGraphNode", "GraphEdgeType", "GraphNodeType",
+    "ICSAttackMapper", "ICSMapping", "MappingRule",
     "CorrelationEdge", "CorrelationEngine", "CorrelationEvidence", "CorrelationWeights",
     "AlertEnricher", "DetectorEvidence", "EnrichedAlert",
     "Incident", "IncidentClusterer", "IncidentLifecycle", "PersistentIncidentStore", "IncidentNotFoundError",
